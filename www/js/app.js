ce517f33b5c8ef1c18cfdec6627c001a2bf1c8e1
Shivang Saxena <Shivang Saxena>
2016-05-23 14:45:42 -0400

    Working

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
 	window.plugins.nativepagetransitions.globalOptions.fixedPixelsTop = 100;
+	window.plugins.nativepagetransitions.globalOptions.duration = 250;
+
 
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
